6b875d37d83bf458cfd400a05f380cb2efd37ac8
leafarmer <leafdevelop@gmail.com>
2018-08-20 13:50:01 +0100

    Okta blog initial structure
    Initials structure based on the tutorial within the Okta blog on
    microservices with Spring Boot
    
    This will lead onto microservice architecture security later

---
+++ beer-catalog-service/src/main/java/uk/co/leafdevelopment/beercatalog/BeerCatalogServiceApplication.java
@@ -0,0 +1,12 @@
+package uk.co.leafdevelopment.beercatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeerCatalogServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeerCatalogServiceApplication.class, args);
+	}
+}
